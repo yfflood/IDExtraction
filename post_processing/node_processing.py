823be3686fc9e23a62af88ecc814299202a43f11
AccuserV <hongyf19@gmail.com>
2024-06-04 14:57:10 +0800

    feat: add combine_nodes, merge nodes identified as coreference with chat_model

--- post_processing/node_processing.py
+++ post_processing/node_processing.py
@@ -22,7 +22,7 @@ def combine_values(node1, node2, chat_model):
     values_merged = []
     for v1 in values1:
         for v2 in values2:
-            if phrase_similarity_llm(v1, v2, chat_model)['synonymy']:
+            if phrase_similarity_llm(v1, v2, chat_model)["synonymy"]:
                 values_merged.append(v1)
                 values1.remove(v1)
                 values2.remove(v2)
@@ -36,39 +36,17 @@ def combine_values(node1, node2, chat_model):
     return values_merged
 
 
-def combine_nodes(node1, node2, chat_model=None):
+def combine_nodes(node1, node2, chat_model):
     """ 
     combine nodes that are **identified** to be identical
     - chat_model: if == None, use manual rules to combine the nodes
     """
-    if chat_model is None:
-        raise NotImplementedError
-    else:
-        # use llm to automatically combine nodes
-        combine_template = """\
-            The two original nodes are considered identical, combine them as a single node in the influence diagram. The new node should have variable_name similar to those of the original nodes, variable_type identical to (at least one of) the original nodes. For values of the original nodes, merge synonymous elements to get the values of the new node.
+    variable_name = node1["variable_name"]
+    variable_type = node1["variable_type"]
+    values = combine_values(node1, node2, chat_model)
+    node_merged = Node(variable_name=variable_name, variable_type=variable_type, values=values)
+    return node_merged
 
-            The original nodes are: {node1} and {node2}.
-
-            Output the node using the following format:
-            {format_instructions}.
-
-            The content should be in the same language as the original nodes.
-        """
-
-        parser = JsonOutputParser(pydantic_object=Node)
-        prompt = PromptTemplate(
-            template = combine_template,
-            input_variables = ["node1", "node2"],
-            partial_variables = {
-                "format_instructions": parser.get_format_instructions(),
-            }
-        )
-
-        chain = prompt | chat_model | parser
-        contents = chain.invoke({"node1": node1, "node2": node2})
-        return contents
-            
         
 def is_coreference(node1, node2, chat_model=None):
     """ 
@@ -106,4 +84,4 @@ if __name__=='__main__':
     ]
     # new_node = combine_nodes(nodes[0],nodes[1],llm)
     #print(new_node)
-    print(combine_values(nodes[0], nodes[1],llm))
+    print(combine_nodes(nodes[0], nodes[1],llm))
